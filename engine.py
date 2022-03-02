66e49f7b0c037d52f8b97463df14943458eca0f5
Arvid Laveno Ling <Laveno@student.chalmers.se>
2022-03-02 14:26:37 +0100

    Engine can be run from command line with six inputs

--- engine.py
+++ engine.py
@@ -1,23 +1,25 @@
+import sys
+
 from controllers.pose.pose_controller import PoseController
 from model.robots.k3lso.k3lso import K3lso 
 
-def set_values(controller):
-    position = [0, 0, 0]
-    orientation = [0, 0, 0]
+def format_values(xpos, ypos, zpos, roll, pitch, yaw):
+    position = [xpos, ypos, zpos]
+    orientation = [roll, pitch, yaw]
     return position, orientation
 
 if __name__ == "__main__":
+    xpos = float(sys.argv[1])
+    ypos = float(sys.argv[2])
+    zpos = float(sys.argv[3])
+    roll = float(sys.argv[4])
+    pitch = float(sys.argv[5])
+    yaw = float(sys.argv[6])
+
     kelso = K3lso(None)
     controller = PoseController(kelso, 0)
-    controller.update_controller_params(set_values(controller))
+    controller.update_controller_params(format_values(xpos, ypos, zpos, roll, pitch, yaw))
     print(controller.get_action())
-
-    # while(True):
-        
-    #     controller.get_action()
-    #     set_values(controller)
-
-    #     print("test")
 
 
 
