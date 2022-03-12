dea4a68165562fe4380ccf77432a4a2346abc230
linawincent <wincent@student.chalmers.se>
2022-03-12 10:45:38 +0100

    Edited test.py for easier printing

--- test.py
+++ test.py
@@ -27,17 +27,24 @@ def get_action(position, orientation):
 
 def convert_pos_ros(command):
     # From radians to relative radians for Ros-commands and sign-change
-    offset = np.array([
+
+    # Since k3lso calculates 0 from defined position
+    offset_motor = np.array([
         -0.03964886725138972, 1.1035218357931036, -1.9554858419361683,
         -0.03964886725138994, 1.1035218357931036, 1.9554858419361683,
         -0.03964886725138972, -1.1035218357931036, -1.9554858419361683,
         -0.03964886725138994, -1.1035218357931036, 1.9554858419361683
         ])
+    # Better zero-position for k3lso
+    offset_orig = np.array([
+        0.017, - 1.030, 1.480, 0.001, - 1.040, 1.430,
+        0.007, - 1.090, 1.460, 0.050, - 1.190, 1.440
+    ])
 
-    transformed_command = np.array(command) - offset
+    transformed_command = np.array(command) - offset_motor + offset_orig
     ids = [1, 2, 5, 6]
-    for i in ids:
-        transformed_command[i] = -transformed_command[i]
+    for j in ids:
+        transformed_command[j] = -transformed_command[j]
 
     return transformed_command
 
@@ -50,7 +57,7 @@ def print_output(pybullet_action, ros_action):
     # print('no. of rotations:')
     # print(ros_action / (2 * 3.1415))
     # print('command:')
-    print('ros2 service call / k3lso_moteus / motors_test k3lso_msgs / srv / MotorsTest '
+    print('ros2 service call /k3lso_moteus/motors_test k3lso_msgs/srv/MotorsTest '
           '"{ids: [1,2,3,4,5,6,7,8,9,10,11,12], position:', np.around(ros_action, 5).tolist(), '}"')
 
 
