e83ed065888c19f20ad80d684d6c0f7d6c2ee8c6
linawincent <wincent@student.chalmers.se>
2022-03-23 17:47:45 +0100

    ros fix

--- pose.py
+++ pose.py
@@ -33,12 +33,12 @@ class Pose:
             -0.05, 0, 0, 0, 0.09, -0.05
         ])
 
-        transformed_command = (np.array(command) - offset_motor + offset_orig) / (2 * 3.14159265)
+        transformed_command = (np.array(command) - offset_motor ) / (2 * 3.14159265)
         ids = [2, 3, 4, 6, 7, 8]
         for j in ids:
             transformed_command[j-1] = -transformed_command[j-1]
 
-        # transformed_command +=
+        transformed_command += offset_orig / (2 * 3.14159265)
         return transformed_command.tolist()
 
     def update_signal(self, position, orientation):
