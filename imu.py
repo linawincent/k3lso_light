1b7331c61d278f40d7142baccba300e2f996eff4
linawincent <wincent@student.chalmers.se>
2022-04-26 10:34:07 +0200

    Error fixes in IMU

--- imu.py
+++ imu.py
@@ -1,12 +1,14 @@
+import time
+
 import numpy as np
 
 
 def q_to_euler(q):
     """ Returns the roll, pitch, yaw from the IMU quaternions """
     orientation = np.zeros(3)
-    orientation[0] = np.arctan(2 * (q[0] * q[1] + q[2] * q[3]) / (1 - 2 * (q[1]**2 * q[2]**2)))
-    orientation[1] = np.arcsin(2 * (q[0] * q[2] - q[1] * q[3]))
-    orientation[2] = np.arctan(2 * (q[0] * q[3] + q[1] * q[2]) / (1 - 2 * (q[2]**2 * q[3]**2)))
+    orientation[0] = np.arctan(2 * (q[3] * q[0] + q[1] * q[2]) / (1 - 2 * (q[0]**2 * q[1]**2)))
+    orientation[1] = np.arcsin(2 * (q[3] * q[1] - q[0] * q[2]))
+    orientation[2] = np.arctan(2 * (q[3] * q[2] + q[0] * q[1]) / (1 - 2 * (q[1]**2 * q[2]**2)))
     return orientation
 
 
@@ -14,13 +16,15 @@ class IMU:
 
     def __init__(self, start_pos):
         # TODO: quaternions from ROS
-        self.q = np.array([0., 0., 0., 0.])
+        self.q = np.array([0., 0., 0., 1.])
         self.angular_vel = np.array([0., 0., 0.])
         self.lin_acc = np.array([0., 0., 0.])
 
         self.position = start_pos
         self.orientation = np.array([0., 0., 0.])
         self.velocity = np.array([0., 0., 0.])
+
+        self.t0 = time.clock()
 
     def get_position(self):
         return self.position
@@ -40,8 +44,10 @@ class IMU:
     def get_q(self):
         return self.q
 
-    def update(self, q, lin_acc, ang_vel, dt):
+    def update(self, q, lin_acc, ang_vel):
         g = 10.1  # gravitational offset for the acceleration in z-direction
+
+        dt = time.clock() - self.t0
 
         """ Update values from IMU"""
         self.q = q
@@ -50,16 +56,16 @@ class IMU:
 
         """ Rotation matrix for q"""
         rot_matrix = np.array([
-            (1 - 2 * (q[2] ** 2 * q[3] ** 2)), 2 * (q[1] * q[2] + q[0] * q[3]), 2 * (q[0] * q[2] + q[1] * q[3]),
-            2 * (q[1] * q[2] + q[0] * q[3]), (1 - 2 * (q[1]**2 * q[3]**2)), 2 * (q[2] * q[3] + q[0] * q[1]),
-            2 * (q[1] * q[3] + q[0] * q[2]), 2 * (q[0] * q[1] + q[2] * q[3]), (1 - 2 * (q[1]**2 * q[2]**2))
+            (1 - 2 * (q[1] ** 2 * q[2] ** 2)), 2 * (q[0] * q[1] + q[3] * q[2]), 2 * (q[3] * q[1] + q[0] * q[2]),
+            2 * (q[0] * q[1] + q[3] * q[2]), (1 - 2 * (q[0]**2 * q[2]**2)), 2 * (q[1] * q[2] + q[3] * q[0]),
+            2 * (q[0] * q[2] + q[3] * q[1]), 2 * (q[3] * q[0] + q[1] * q[2]), (1 - 2 * (q[0]**2 * q[1]**2))
         ])
         rot_matrix = rot_matrix.reshape((3, 3))
 
         """ Calculate position and velocity in world frame"""
         self.orientation = q_to_euler(q)
         self.velocity += np.matmul(rot_matrix, self.lin_acc) * dt
-        self.position += self.velocity * dt + 0.5 * np.matmul(rot_matrix, self.lin_acc) * dt * dt
+        self.position += self.velocity * dt  # + 0.5 * np.matmul(rot_matrix, self.lin_acc) * dt * dt
 
 
 
